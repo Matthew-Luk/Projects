106cb94fb4586d27eb07c28c628672a3237dc827
Matthew Luk <Matthew.Luk@yahoo.com>
2023-01-07 20:47:14 -0800

    added front end and worked on some class objects

--- scrape/client/src/App.js
+++ scrape/client/src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
+import {BrowserRouter, Routes, Router, Route, Navigate} from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import Login from './components/Login';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(1)
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/time")
     .then(res => res.json())
     .then(data => {
       setCurrentTime(data.time)
+      console.log(data)
     })
   },[])
 
   return (
     <div className="App">
-      <p>The current time is {currentTime}.</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to ='/login'/>}/>
+          <Route path='/login' element={<Login firstName={firstName} setFirstName={setFirstName} lastName={lastName} setLastName={setLastName}/>}/>
+          <Route path='/home' element={<Home />}/>
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
