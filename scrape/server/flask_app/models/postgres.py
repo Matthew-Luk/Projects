f33f08ec5ca29946e7fbf3f3ecdc8137c97c9a48
Matthew Luk <Matthew.Luk@yahoo.com>
2023-01-11 22:07:57 -0800

    matthew/rename-files-and-clean-up-code

--- scrape/server/flask_app/models/postgres.py
+++ scrape/server/flask_app/models/postgres.py
@@ -3,11 +3,11 @@ import psycopg2
 
 class Postgres:
     def __init__(self,
-                 host: str = "localhost",
-                 user: str = "postgres",
-                 password: str = "password",
-                 database: str = "scrape",
-                 port: int = 5432):
+                host: str = "localhost",
+                user: str = "postgres",
+                password: str = "password",
+                database: str = "scrape",
+                port: int = 5432):
         try:
             conn = psycopg2.connect(host=host, user=user, password=password, database=database, port=port)
         except:
--- scrape/server/flask_app/models/user.py
+++ scrape/server/flask_app/models/user.py
@@ -8,7 +8,3 @@ class User:
         self.email = data["email"]
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
-
-    @classmethod
-    def get_all(cls):
-        pass
