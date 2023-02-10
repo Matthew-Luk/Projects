7ac5c54f77a54372ec6edc39d1cfb5f9935178d1
Matthew Luk <Matthew.Luk@yahoo.com>
2023-02-09 19:26:16 -0800

    remove build folder from repo

--- build/lib/scrape/server/flask_app/models/watchlist.py
+++
@@ -1,7 +0,0 @@
-from flask_app.configuration import Config
-from scrape.server.flask_app.models import Product as ProductModel
-
-class Watchlist:
-    def __init__(self,data):
-        self.user_id = data["user_id"]
-        self.products: list[ProductModel] = []
