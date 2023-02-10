a32f46ffb195460bec931e873f24e11d18f9fcf9
ray-homepc <rliu245@gmail.com>
2023-02-10 13:12:52 -0800

    create Controller for sharing vars across routes

--- scrape/server/flask_app/controllers/products.py
+++ scrape/server/flask_app/controllers/products.py
@@ -1,10 +1,8 @@
 from flask_app import app
-from scrape.server.flask_app.repo.products import Product as ProductRepo
-from scrape.server.flask_app.configuration import Config
+from scrape.server.flask_app.controllers.controllers import Controller
 
 @app.route("/top_searches")
 def top_searches():
-    config = Config()
-    p = ProductRepo(config)
+    p = Controller.get_product_repo()
     top_searches = p.get_top_searches()
     return {"top_searches": top_searches}
